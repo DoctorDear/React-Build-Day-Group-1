3f1620826f002560b2db9adb348f930aae6c72a2
Chirasak Tapphae <dearchirasak@gmail.com>
2026-08-20 15:55:16 +0700

    feat: add initial navigation menu data structure

--- src/data/menus.js
+++ src/data/menus.js
@@ -7,4 +7,84 @@ export default [
     imageUrl:
       "https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?auto=format&fit=crop&w=600&q=80",
   },
+  {
+    id: "food-02",
+    name: "กะเพราหมูกรอบไข่ดาว",
+    type: "food",
+    category: "ข้าว",
+    imageUrl:
+      "https://images.unsplash.com/photo-1569058242567-93de6f36f8e6?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "food-03",
+    name: "ข้าวมันไก่ต้ม",
+    type: "food",
+    category: "ข้าว",
+    imageUrl:
+      "https://images.unsplash.com/photo-1626804475297-41608ea09aeb?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "food-04",
+    name: "ก๋วยเตี๋ยวต้มยำกุ้ง",
+    type: "food",
+    category: "เส้น",
+    imageUrl:
+      "https://images.unsplash.com/photo-1559847844-5315695dadae?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "food-05",
+    name: "ข้าวหน้าเนื้อญี่ปุ่น (Gyudon)",
+    type: "food",
+    category: "ข้าว",
+    imageUrl:
+      "https://images.unsplash.com/photo-1588166524941-3bf61a9c41db?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "food-06",
+    name: "สุกี้น้ำรวมมิตร",
+    type: "food",
+    category: "ต้ม/แกง",
+    imageUrl:
+      "https://images.unsplash.com/photo-1547592180-85f173990554?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "drink-01",
+    name: "ชาไทยเย็นโบราณ",
+    type: "drink",
+    category: "ชา",
+    imageUrl:
+      "https://images.unsplash.com/photo-1576092768241-dec231879fc3?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "drink-02",
+    name: "มัทฉะลาเต้เย็น",
+    type: "drink",
+    category: "ชา",
+    imageUrl:
+      "https://images.unsplash.com/photo-1536256263959-770b48d82b0a?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "drink-03",
+    name: "อเมริกาโน่เย็น",
+    type: "drink",
+    category: "กาแฟ",
+    imageUrl:
+      "https://images.unsplash.com/photo-1517701550927-30cf4ba1dba5?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "drink-04",
+    name: "ชามะนาวเย็น",
+    type: "drink",
+    category: "ชา",
+    imageUrl:
+      "https://images.unsplash.com/photo-1556679343-c7306c1976bc?auto=format&fit=crop&w=600&q=80",
+  },
+  {
+    id: "drink-05",
+    name: "นมสดคาราเมลเย็น",
+    type: "drink",
+    category: "นม",
+    imageUrl:
+      "https://images.unsplash.com/photo-1572490122747-3968b75cc699?auto=format&fit=crop&w=600&q=80",
+  },
 ];
